7a3df1b242ddbed16abe85f1d4fb9d1a6a05996d
Vitalii <nevvord@gmail.com>
2019-06-10 23:22:24 +0300

    Это первый комит

---
+++ t2/folder/js/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone.name, persone.age, persone.isMarried);
+
+let arr = ['plug.png','lol.png'];
+
+console.log(arr[0], arr[1]);
+
+//let infoConfirm = confirm("Are you here?");
+
+//console.log("infa confirma = ", infoConfirm);
+
+let answer = prompt("Are you here?");
+
+console.log(typeof(arr));
+
