19a99eccc2ea4019a9693e8ce741a99d38cf325a
Vitalii <nevvord@gmail.com>
2019-06-12 10:22:46 +0300

    Все до ООП

--- t8/folder/script.js
+++ t8/folder/script.js
@@ -100,3 +100,111 @@ console.log(parseFloat(twelve));
 
 
 
+
+let data = {
+    login: "nevvord",
+    password: "0000",
+    mail: "a@a.a",
+    avtoriz: false,
+    game: {
+        id: 1,
+        name: "nevvord",
+        country: "gr",
+        city: {
+            c1: {
+                id: 1,
+                name: "Odessos",
+                def: 100,
+                residents: 5,
+                resources: {
+                    gold: 0,
+                    wood: 25,
+                    food: 15
+                }
+            }
+        }
+    }
+}
+
+data.game.city.c1.resources.stone = 25;
+
+//delete data.game.city.c1.resources.food;
+
+//console.log(data.game.city.c1.resources);
+
+
+for (const key in data) {
+    console.log('Свойсто ' + key + ' имеет значение ' + data[key]);   
+}
+console.log(Object.keys(data).length);
+
+
+console.log(data);
+
+
+let arr = [1, 2, 3, 4, 5];
+
+arr.pop();          // Удаляет последнюю строку в масиве
+arr.push('5');      // Добавляет в конец масива
+
+arr.shift();        // Удаляет первую строку масива
+arr.unshift('1');   // Добавляет первую строку масива
+
+/*
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+*/
+
+// Вывод элементов масива удобненько **
+
+arr.forEach(function (item, i, massiv) {
+    console.log(i + ': ' + item + ' (масив: ' + massiv + ')');
+    
+})
+
+// -------------------------------------
+
+let mass = [1, 3, 4, 6, 7];
+
+for (let key in mass){
+    console.log(key);
+    
+}
+
+
+console.log(arr);
+
+/* Создание масива и запись из всплывающего окна
+let ans = prompt("", ""),
+    arrayAns = [];
+
+arrayAns = ans.split(',');
+
+console.log(arrayAns);
+*/
+
+/*
+// Записуем все в строку
+let arraySend = ["a", "b", "c", "ew"],
+    iSend = arraySend.join(", ");
+
+console.log(iSend);
+*/
+
+/*
+// Сортировка элементов масива с записью в переменную 
+let arraySend = ["b", "z", "f", "ew", "a"],
+    iSend = arraySend.sort();
+
+console.log(iSend);
+*/
+
+let arraySend = ["1", "3", "16", "27", "100"],
+    iSend = arraySend.sort(compareNum);
+
+function compareNum(a, b) {
+    return a-b;
+}
+
+console.log(iSend);
