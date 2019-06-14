1a7ff01b4fc71f588a0620a7430838120319922a
Vitalii <nevvord@gmail.com>
2019-06-14 11:01:17 +0300

    таймеры события т тд

--- sobitiya/folder/js.js
+++ sobitiya/folder/js.js
@@ -10,7 +10,7 @@ btn[0].onclick = function () {
     alert("Your press fearst button");
 };
 */
-
+/*
 btn[0].addEventListener('click', function(event){
     let varTar = event.target;
 
--- sobitiya/folder/time.js
+++ sobitiya/folder/time.js
@@ -11,8 +11,48 @@ function sayHelo() {
     console.log("Hello World!");
 };
 */
-
+/*
 let timerId = setTimeout(function log(){
     console.log("Hello Woeld");
     setTimeout(log, 2000);
+});
+*/
+let smallBox = document.querySelector('.smallBox'),
+    btnMoveBox = document.querySelector('#moveBox');
+
+function anima() {
+    let pos = 0;
+
+    let timerMove = setInterval(moveBox, 10);
+
+    function moveBox() {
+        if (pos == 300) {
+            clearInterval(timerMove);
+        }else{
+            pos++;
+            smallBox.style.top = pos + 'px';
+            smallBox.style.left = pos + 'px';
+        }
+    }   
+}
+
+moveBox.addEventListener('click', anima);
+
+let btnBlock = document.querySelector('.btn-block'),
+    btns = document.getElementsByTagName('button');
+
+    /*
+    console.log("Блок с конпками");
+    console.log(btnBlock);
+    console.log("Кнопки");
+    console.log(btns);
+    */
+    
+
+btnBlock.addEventListener('click', function(event) {
+    //console.log(event);
+    
+   if (event.target && event.target.matches('button.first')) {
+        console.log("Hello"); 
+   } 
 });
